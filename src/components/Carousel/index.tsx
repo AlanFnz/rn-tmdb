71ffe382273c7383ad2c0044a1b29c5c9c37da33
Alan Fernandez <alan.f@msn.com>
2024-08-27 20:54:08 +0200

    basic carousel implementation

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,8 +1,63 @@
 import React from 'react';
+import styled from 'styled-components/native';
 import { Text } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { useGetMoviesByGenreQuery } from '../../store/services/tmdbApi';
 
-const Carousel = ({ movieName }: { movieName: string }) => {
-  return <Text>{movieName}</Text>;
-};
+interface CarouselProps {
+  genreId: number;
+  genreName: string;
+}
 
-export default Carousel;
+export default function Carousel({ genreId, genreName }: CarouselProps) {
+  const { data: movies, error, isLoading } = useGetMoviesByGenreQuery(genreId);
+
+  if (isLoading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error loading movies.</Text>;
+
+  return (
+    <Container>
+      <GenreName>{genreName}</GenreName>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {movies &&
+          movies.map(movie => (
+            <MovieCard key={movie.id} onPress={() => {}}>
+              <Poster
+                source={{
+                  uri: `https://image.tmdb.org/t/p/w200/${movie.poster_path}`, // FIXME: would be nice to not have to hardcode the first part of the url
+                }}
+              />
+              <MovieTitle>{movie.title}</MovieTitle>
+            </MovieCard>
+          ))}
+      </ScrollView>
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  margin-vertical: 10px;
+`;
+
+const GenreName = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+const MovieCard = styled.TouchableOpacity`
+  margin-right: 10px;
+  align-items: center;
+`;
+
+const Poster = styled.Image`
+  width: 100px;
+  height: 150px;
+  border-radius: 8px;
+`;
+
+const MovieTitle = styled.Text`
+  text-align: center;
+  width: 100px;
+  margin-top: 5px;
+`;
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import styled from 'styled-components/native';
+import { Text, View } from 'react-native';
+import { useGetGenresQuery } from '../../store/services/tmdbApi';
+import Carousel from '../../components/Carousel';
 
-const HomeScreen = () => {
-  return <></>;
-};
+export default function HomeScreen() {
+  const { data: genres, error, isLoading } = useGetGenresQuery();
 
-export default HomeScreen;
+  if (isLoading) return <Text>Loading...</Text>;
+  if (error) return <Text>Failed to load genres.</Text>;
+
+  // arbitrarily extracting three genres in order to meet the challenge requirement
+  const selectedGenres = genres && genres.genres.slice(0, 3);
+
+  return (
+    <View>
+      {selectedGenres && selectedGenres.length > 0 ? (
+        selectedGenres.map(genre => (
+          <Carousel key={genre.id} genreId={genre.id} genreName={genre.name} />
+        ))
+      ) : (
+        <StyledText>No genres available.</StyledText>
+      )}
+    </View>
+  );
+}
+
+const StyledText = styled.Text`
+  font-size: 18px;
+  text-align: center;
+  margin-vertical: 10px;
+`;
