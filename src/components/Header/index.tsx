104fed3df6cf8917f8e51ba9a407b97b5b761baa
Alan Fernandez <alan.f@msn.com>
2024-08-28 01:23:12 +0200

    added back button on header for film detail screen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import styled from 'styled-components/native';
 
 export default function Header() {
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const isFilmDetailScreen = route.name === 'MovieDetailsScreen';
+
   return (
     <HeaderContainer>
+      {isFilmDetailScreen && (
+        <BackButton onPress={() => navigation.goBack()}>
+          <BackButtonText>{'<'}</BackButtonText>
+        </BackButton>
+      )}
       <HeaderText>FilmApp</HeaderText>
     </HeaderContainer>
   );
@@ -15,10 +26,24 @@ const HeaderContainer = styled.View`
   justify-content: flex-end;
   align-items: center;
   padding-bottom: 10px;
+  flex-direction: column;
 `;
 
 const HeaderText = styled.Text`
   font-size: 24px;
   color: white;
   font-weight: bold;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const BackButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 20px;
+  bottom: 12px;
+`;
+
+const BackButtonText = styled.Text`
+  font-size: 22px;
+  color: white;
 `;
