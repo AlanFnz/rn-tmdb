5a9eb8c260376659b3953a7ab903b5acc23947ad
Alan Fernandez <alan.f@msn.com>
2024-08-27 19:28:35 +0200

    carousel base component and prettier config update

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Carousel = ({ movieName }: { movieName: string }) => {
+  return <Text>{movieName}</Text>;
+};
+
+export default Carousel;
