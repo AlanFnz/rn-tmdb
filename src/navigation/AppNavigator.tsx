7469cbc23ea856bec8d47b3782e6d366f1a525e1
Alan Fernandez <alan.f@msn.com>
2024-08-27 20:42:12 +0200

    created navigation types an typed useNavigation hook

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
 import HomeScreen from '../screens/HomeScreen';
 import WishListScreen from '../screens/WishListScreen';
 import FilmDetailScreen from '../screens/FilmDetailScreen';
@@ -18,5 +20,14 @@ const AppNavigator = () => {
     </NavigationContainer>
   );
 };
+
+export type RootStackParamList = {
+  Home: undefined;
+  MovieDetailsScreen: { movieId: number };
+  Wishlist: undefined;
+};
+
+export type NavigationProp = NativeStackNavigationProp<RootStackParamList>;
+export const useAppNavigation = () => useNavigation<NavigationProp>();
 
 export default AppNavigator;
